e4959571db91646947a54544444e60091b9c2582
haroonahmad256 <ha9417293@gmail.com>
2026-01-03 09:07:08 +0500

    Added Struct Login forgot password function

--- src/admin.c
+++ src/admin.c
@@ -4,6 +4,19 @@
 // Aur function prototype .h file me likein uper ki do lines aur end wali line k middle me
 #include <stdio.h>
 #include <string.h>
+struct students {
+    char student_id[20];
+    char student_name[100];
+    char password[50];
+    char department[50];
+};
+
+struct admins
+{
+    char admin_id[20];
+    char admin_password[50];
+};
+
 
 
 
@@ -15,4 +28,124 @@ void adminMenu()
     printf("4. Manage Grades");
     printf("5. Logout");
 }
+
+void Login_admin()
+{
+
+    int choice;
+    printf("1. Login as Admin\n");
+    printf("2. Reset Password\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        FILE *fptr;
+        fptr = fopen("admin_login.txt", "r");
+
+        if (fptr == NULL)
+        {
+            printf("Error opening file");
+        }
+
+        char enter_adminid[50];
+        char enter_password[20];
+        printf("Enter your Admin Id: ");
+        scanf("%s", enter_adminid);
+        printf("Enter your password: ");
+        scanf("%s", enter_password);
+
+        int i = 1;
+        do
+        {
+            int on = 0;
+            struct admins admin;
+
+            while (fscanf(fptr, "%s %s", admin.admin_id, admin.admin_password) == 2)
+            {
+
+                if (strcmp(enter_adminid, admin.admin_id) == 0 && strcmp(enter_password, enter_adminid) == 0)
+                {
+                    on = 1;
+                }
+            }
+
+            if (on)
+            {
+                printf("Login successful\n");
+                i = 0;
+            }
+
+            else
+            {
+                printf("Invaid admin id or password\n");
+                break;
+            }
+
+            fclose(fptr);
+
+        } while (i);
+
+        break;
+
+    case 2:
+
+        rewind(fptr);
+        FILE *fptr2;
+        fptr = fopen("admin_login.txt", "r");
+        fptr2 = fopen("admin_login_temp.txt", "a");
+        char forgotpassword;
+        char admin_id[4], new_password[10];
+        struct admins admin;
+
+        printf("Forgot password as Admin(y/n): ");
+        scanf("%s", forgotpassword);
+        int inside = 0;
+
+        if (forgotpassword == 'y' || forgotpassword == 'Y')
+        {
+            printf("Enter your Admin ID to verify: ");
+            scanf("%s", admin_id);
+
+            while (fscanf(fptr, "%s %s", admin.admin_id, admin.admin_password) == 2)
+            {
+
+                if (strcmp(admin_id, admin.admin_id) == 0)
+                {
+                    printf("Enter new password: ");
+                    scanf("%s", new_password);
+                    fprintf(fptr2, "%s %s", admin.admin_id, new_password);
+                    inside = 1;
+                }
+
+                else
+                {
+
+                    fprintf(fptr2, "%s %s", admin.admin_id, admin.admin_password);
+                }
+            }
+
+            fclose(fptr2);
+            fclose(fptr);
+
+            if (inside)
+            {
+                remove("admin_login.txt");
+                rename("admin_login_temp.txt", "admin_login.txt");
+                printf("Your password has been changed successfully");
+            }
+
+            else
+            {
+                remove("admin_login_temp.txt");
+                printf("Pssword could not changed");
+            }
+        }
+        break;
+    default:
+        break;
+    }
+}
+
 
